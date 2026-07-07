ea196e85d044e325e09399a0bbe9fab29646078f
Alexander KIRPICHNIKOV <109668915+d78714@users.noreply.github.com>
2026-07-07 11:25:13 +0300

    first commit

---
+++ src/main/java/com/kirpichnikov/everydayleetcode/EverydayleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.kirpichnikov.everydayleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayleetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverydayleetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirpichnikov/everydayleetcode/EverydayleetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirpichnikov.everydayleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverydayleetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
